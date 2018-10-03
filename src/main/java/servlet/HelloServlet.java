cfaa51856d87282ec89bae44b092235c3a72eb5a
mick <michaeldu91@hotmail.fr>
2018-10-03 08:57:46 +0200

    Deploy

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -35,7 +35,7 @@ public class HelloServlet extends HttpServlet {
             Connection c =  Database.getConnection();
             PreparedStatement ps = c.prepareStatement("SELECT * FROM Jojo_Currency");
             ResultSet resultSet = ps.executeQuery();
-            out.println("------SQL----------");
+            out.println("\n------SQL----------");
             if(resultSet.next()){
                 out.println("\n"+resultSet.getString(1));
                 out.println(resultSet.getDouble(2));
