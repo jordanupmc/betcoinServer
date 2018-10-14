7a4033718a922424554557944f4657d2bf66829e
JustinRudat <justin.rudat@etu.upmc.fr>
2018-10-14 18:21:41 +0200

    adding addbet service

---
+++ src/main/java/services/AddBetService.java
@@ -0,0 +1,44 @@
+package services;
+
+import bd.BetTools;
+import bd.SessionTools;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+
+import static bd.BetTools.canCancelBet;
+import static bd.UserTools.userConnected;
+import static services.ServiceTools.*;
+
+public class AddBetService {
+    public static JSONObject addBet(String token, String login, String idPool, String ammount, String value) {
+        if ((login == null) || (idPool == null) || (ammount == null) || (value == null)) {
+            return serviceKO("AddBet Fail : Wrong arguments, expecting: login idPool ammount value");
+        }
+
+        boolean connected = userConnected(login);
+        if (!connected) return serviceKO("AddBet Fail : User not connected");
+
+        if (!SessionTools.checkToken(token, login)) {
+            return serviceKO("AddBet Fail : Wrong token");
+        }
+
+        try {
+            if (canCancelBet(idPool)) {
+                return serviceKO("AddBet Fail : Too late to add a new bet");
+            }
+
+        } catch (SQLException e) {
+            return serviceKO("AddBet Fail : SQLException");
+
+        } catch (URISyntaxException e) {
+            return serviceKO("AddBet Fail : URISyntaxException");
+
+        }
+        if (BetTools.addBet(login, idPool, Float.parseFloat(ammount), Float.parseFloat(value)))
+            return serviceOK();
+
+        return serviceKO("AddBet Fail : Unkwown Error");
+    }
+}
