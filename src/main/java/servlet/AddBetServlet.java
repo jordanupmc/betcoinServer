415f07e60363f2713bba8aa88a718006bf2d2d15
JustinRudat <justin.rudat@etu.upmc.fr>
2018-10-14 18:22:00 +0200

    adding addbet servlet

--- src/main/java/servlet/AddBetServlet.java
+++ src/main/java/servlet/AddBetServlet.java
@@ -1,4 +1,47 @@
 package servlet;
 
-public class AddBetServlet {
+import services.AddBetService;
+import services.CancelBetService;
+import services.LoginService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        name = "AddBetServlet",
+        urlPatterns = {"/addBet"}
+)
+public class AddBetServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType( "text / plain" );
+        PrintWriter out = resp.getWriter();
+
+
+        String login=req.getParameter("login");
+        String idPool=req.getParameter("idPool");
+        String ammount=req.getParameter("betAmmount");
+        String value = req.getParameter("betValue");
+        String token = req.getParameter("token");
+
+
+        out.print(
+                AddBetService.addBet(token,login, idPool, ammount,value)
+        );
+
+        out.close();
+    }
 }
