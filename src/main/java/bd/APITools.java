9688ecc0ebb0207f40eb12572b0cffd127fe2564
JustinRudat <justin.rudat@etu.upmc.fr>
2018-10-16 14:59:22 +0200

    converting timestamp to date to get hours limit

--- src/main/java/bd/APITools.java
+++ src/main/java/bd/APITools.java
@@ -9,26 +9,35 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class APITools {
-    private static String createURL(String cryptName, String devise, String fin, String debut){
+    private static String createURL(String cryptName, String devise, String fin, String debut) throws ParseException {
         Timestamp tmstp_fin = new Timestamp(Timestamp.parse(fin));
         Timestamp tmstp_debut = new Timestamp(Timestamp.parse(debut));
+
+
+        Date d1 = new Date(tmstp_fin.getTime());
+        Date d2 = new Date(tmstp_debut.getTime());
+        int diffInHours = (int)(Math.abs(d1.getTime() - d2.getTime())/3600000);
 
         String retour = "https://min-api.cryptocompare.com/data/histohour?fsym=" +
                 cryptName +
                 "&tsym=" +
                 devise +
                 "&limit=" +
-                fin +
+                diffInHours +
                 "&toTs=" +
-                "";
+                tmstp_fin;
 
         return retour;
     }
 
-    public static JSONObject tCrypto(String cryptName, String devise, String fin, String debut) throws IOException {
+    public static JSONObject tCrypto(String cryptName, String devise, String fin, String debut) throws IOException, ParseException {
         String source ="";
         String url = createURL(cryptName,devise,fin,debut);
         URL oracle = new URL(url);
