b10389fb3a83ece273f3f2e8e8562de0c92ff457
JustinRudat <justin.rudat@etu.upmc.fr>
2018-10-18 14:15:45 +0200

    final fix for the timestamp : IT WAS WORKING ALL ALONG

--- src/main/java/bd/APITools.java
+++ src/main/java/bd/APITools.java
@@ -1,24 +1,32 @@
 package bd;
 
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.sql.Time;
 import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 
 public class APITools {
     private static String createURL(String cryptName, String devise, String fin, String debut){
-        Timestamp tmstp_fin = new Timestamp(Long.parseLong(fin));
-        Timestamp tmstp_debut = new Timestamp(Long.parseLong(debut));
+        long tmsp_fin = Long.parseLong(fin)*1000;
+        long tmsp_debut = Long.parseLong(debut)*1000;
+
+        long diff = tmsp_fin - tmsp_debut;
+        long diffInHours = Math.abs((diff / (60*60*1000))%24);
+        long diffInDays = Math.round(diff / (60*60*1000)/24);
+        long diffTot = 24*diffInDays + diffInHours;
 
 
-        Date d1 = new Date(tmstp_fin.getTime());
-        Date d2 = new Date(tmstp_debut.getTime());
-        int diffInHours = (int)(Math.abs(d1.getTime() - d2.getTime())/3600000);
 
 
 
@@ -27,10 +35,10 @@ public class APITools {
                 "&tsym=" +
                 devise +
                 "&limit=" +
-                diffInHours +
+                Math.abs(diffTot) +
                 "&toTs=" +
-                tmstp_fin.getTime();
-
+                tmsp_fin;
+        System.out.println(diffTot);
         return retour;
     }
 
