5dd0f475b427d0d32bed7da9e7e09547035e9fcc
jordan.ji-ji@live.fr <jordan.ji-ji@live.fr>
2018-10-22 17:52:12 +0200

    connect + subscribe en POST

--- src/main/java/servlet/ConnectServlet.java
+++ src/main/java/servlet/ConnectServlet.java
@@ -1,5 +1,6 @@
 package servlet;
 
+import org.json.JSONObject;
 import services.LoginService;
 
 import javax.servlet.ServletException;
@@ -7,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.ValidationException;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -20,14 +22,32 @@ public class ConnectServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req,resp);
+
+        PrintWriter out = resp.getWriter();
+        JSONObject j = ValidatorHelper.getJSONParameter(req,resp);
+
+        if(j!=null) {
+            try {
+                String login = ValidatorHelper.getParam(j, "login", true);
+                String mdp = ValidatorHelper.getParam(j, "password", true);
+                out.print(
+                        LoginService.connect(login, mdp)
+                );
+            }catch(Exception e){
+                out.print(serviceKO(e.getMessage()));
+            }
+        }
+        else {
+            out.print(serviceKO("Aucune parametre recu"));
+        }
+        out.close();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        resp.setContentType( "text / plain" );
+        resp.setContentType("application/json");
         PrintWriter out = resp.getWriter();
 
 
