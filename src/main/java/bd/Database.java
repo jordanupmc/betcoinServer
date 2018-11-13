2b09df7dcc9dad5d5e1c99c33980da51e8127368
jordan.ji-ji@live.fr <jordan.ji-ji@live.fr>
2018-11-13 21:09:12 +0100

    try remove mdp more security

--- src/main/java/bd/Database.java
+++ src/main/java/bd/Database.java
@@ -14,22 +14,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-
-    public static final String mongoURI = "mongodb://betcoin:betcoin2018@ds125673.mlab.com:25673/heroku_7kxfs0jk";
     private static MongoDatabase mongoDB = null;
 
     private Database(){
     }
-    /*
-    * ntzmikexhujzmd psw = a4ef7dd8a60e2e952477f2706492458bcdabfe44ede05ed8714a59fabc19a72e
-jdbc:postgresql://ec2-54-225-68-133.compute-1.amazonaws.com/ds85knecevckl?sslmode=require
-    * */
 
     /* renvois une connection à la base de données postgreSQL */
     public static Connection getConnection() throws URISyntaxException, SQLException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
-        /*URI dbUri = new URI("postgres://ntzmikexhujzmd:a4ef7dd8a60e2e952477f2706492458bcdabfe44ede05ed8714a59fabc19a72e@ec2-54-225-68-133.compute-1.amazonaws.com:5432/ds85knecevckl"
-        );*/
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
 
@@ -40,7 +32,8 @@ jdbc:postgresql://ec2-54-225-68-133.compute-1.amazonaws.com/ds85knecevckl?sslmod
     /* Renvoi la collection mongoDB correspondant au parametre */
     public static MongoCollection<Document> getMongoCollection(String nomColl){
         if(mongoDB == null) {
-            MongoClientURI uri = new MongoClientURI(Database.mongoURI);
+
+            MongoClientURI uri = new MongoClientURI(System.getenv("MONGODB_URI"));
             MongoClient client = new MongoClient(uri);
             mongoDB = client.getDatabase(uri.getDatabase());
         }
